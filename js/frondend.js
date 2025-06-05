f5e7d9b09a64248e26d996a1a797b6e2378b3cc9
KhonThanh <tangkhonthanh@gmail.com>
2025-06-05 17:02:12 +0700

    working
    Html
    css
    js

---
+++ js/frondend.js
@@ -0,0 +1,86 @@
+// js phần component
+document.addEventListener("DOMContentLoaded", () => {
+  const includeElements = document.querySelectorAll("[data-include]");
+
+  includeElements.forEach(async el => {
+    const file = el.getAttribute("data-include");
+    if (!file) return;
+
+    try {
+      const response = await fetch(`${file}?v=${Date.now()}`, { cache: "no-store" });
+      if (!response.ok) throw new Error(`Không tìm thấy file: ${file}`);
+
+      const text = await response.text();
+      el.innerHTML = text;
+
+      if (typeof initResponsive === "function") {
+        initResponsive(el);
+      }
+
+    } catch (err) {
+      el.innerHTML = `
+        <div style="color: red; padding: 1rem; background: #fff0f0; border: 1px solid red;">
+          ⚠ Không tải được component: <strong>${file}</strong>
+        </div>
+      `;
+      console.error("Lỗi khi fetch:", file, err);
+    }
+  });
+});
+
+// js thêm width và height vào bất kì thẻ img
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("img");
+
+  images.forEach((img) => {
+    if (!img.hasAttribute("loading")) {
+      img.setAttribute("loading", "lazy");
+    }
+    if (img.complete) {
+      setDimensions(img);
+    } else {
+      img.addEventListener("load", function () {
+        setDimensions(img);
+      });
+    }
+  });
+
+  function setDimensions(img) {
+    if (!img.hasAttribute("width")) {
+      img.setAttribute("width", img.naturalWidth);
+    }
+    if (!img.hasAttribute("height")) {
+      img.setAttribute("height", img.naturalHeight);
+    }
+  }
+});
+
+// js banner chỗ trang chủ
+$(document).ready(function () {
+  const $banner = $(".slidebox");
+
+  if ($banner.length) {
+    const $images = $banner.children("div");
+    const currentCount = $images.length;
+
+    if (currentCount < 2) {
+      const clonesNeeded = 3 - currentCount;
+
+      for (let i = 0; i < clonesNeeded; i++) {
+        const $clone = $images.eq(0).clone();
+        $banner.append($clone);
+      }
+    }
+    $banner.slick({
+      infinite: true,
+      speed: 2000,
+      dots: false,
+      autoplay: true,
+      arrows: false,
+      touchMove: false,
+      fade:true,
+    });
+  } else {
+    console.log("Không tìm thấy .slide-cmt – Slick không khởi tạo.");
+  }
+});
